54870f05c8608776bda540ddf7c08467a2f53147
santiago <santiago.vasquez@rappi.com>
2020-08-16 13:10:41 -0500

    Processed data

--- src/data_processing.R
+++ src/data_processing.R
@@ -20,8 +20,7 @@ df <- do.call("rbind", data_list)
 # --------------------------- data cleansing -----------------------------
 
 # Drop the columns of the dataframe
-df <- select (df,-c(X,Y,OBJECTID,RADICADO,CBML,MES_NOMBRE,
-                    X_MAGNAMED,Y_MAGNAMED, BARRIO,DIRECCION_ENC))
+df <- select (df,-c(X,Y,OBJECTID,RADICADO,MES_NOMBRE,X_MAGNAMED,Y_MAGNAMED))
 
 # Lowcase rownames
 df <- mutate_each(df, funs=tolower)
@@ -34,11 +33,28 @@ df <- df %>%
 df <- df %>% 
   mutate(FECHA = as.Date.character(FECHA, tryFormats = c("%Y-%m-%d", "%Y/%m/%d")))
 
-# Remove empty rows
-df <- df[df$CLASE!= "", ]
-df <- df[df$DISENO!= "", ]
-df <- df[df$COMUNA!= "", ]
-df <- df[df$COMUNA!= '0', ]
+## CLASE
+accident_class <- table(df$CLASE)
+unique_accident_class <- names(accident_class)
+
+extract_str <- function(str_vector, regex){
+  idx <- stringi::stri_detect(str = str_vector, regex = regex)
+  return(str_vector[idx])
+}      
+# caida ocupante
+one_class <- extract_str(str_vector = unique_accident_class, regex = "[O|o]cupante$")
+df$CLASE <- ifelse(df$CLASE %in% one_class, "caida ocupante", df$CLASE)
+
+accident_class <- table(df$CLASE)
+unique_accident_class <- names(accident_class)
+
+# choque
+(two_class <- extract_str(str_vector = unique_accident_class, regex = "[C|c]hoque"))
+df$CLASE <- ifelse(df$CLASE %in% two_class, "choque", df$CLASE)
+
+# Otros
+df$CLASE <- ifelse(df$CLASE == "", "otro", df$CLASE)
+df$CLASE <- tolower(df$CLASE)
 
 # Select data frame training and testing
 train_data <- df[df$PERIODO!= '2018', ]

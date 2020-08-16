eab343f52c52f792264e00811c8db3586a4e416b
Alejandro <alejandroc.cadavid@rappi.com>
2020-08-15 23:44:04 -0500

    Changed raw files and data preprocessing file

--- src/data_processing.R
+++ src/data_processing.R
@@ -1,26 +1,61 @@
 # --------------------------- Reading data files -----------------------------
+library(data.table)
+library(stringi)
+
+filePaths <- list.files(file.path("./data/raw"), pattern = 'Acc', full.names = TRUE)
+result <- lapply(filePaths, fread)
+names(result) <- filePaths
+accidentes <- rbindlist(result, use.names = TRUE, idcol = "source")
+accidentes <- accidentes[,source:=NULL]
+
 
 ## All the data files
 raw_files <- list.files('data/raw/')
 
-## Data for exploratory data analysis and training models
-train_raw_files <- raw_files[1:(length(raw_files) - 1)]
-## test data
-test_raw_files <- raw_files[length(raw_files)]
-
 ## building the data paths
-train_data_files <- sapply(
-  train_raw_files
+data_files <- sapply(
+  raw_files
   , function (x) paste0("./data/raw/", x)
   , USE.NAMES = F
   )
 
 ## Building the dataframes
-train_data_list <- lapply(train_data_files, read.csv)
-train_data <- do.call("rbind", train_data_list)
-test_data <- read.csv(file = paste0("./data/raw/", test_raw_files))
+data_list <- lapply(data_files, function(x) read.csv(file = x))
+df <- do.call("rbind", data_list)
+
+# --------------------------- data cleansing -----------------------------
+str(df)
+
+## CLASE
+table(df$CLASE)
+unique_accident_class <- unique(accident_class)
+
+extract_str <- function(str_vector, regex){
+  idx <- stringi::stri_detect(str = str_vector, regex = regex)
+  return(str_vector[idx])
+}      
+# caida ocupante
+one_class <- extract_str(str_vector = unique_accident_class, regex = "[O|o]cupante$")
+df$CLASE <- ifelse(df$CLASE %in% one_class, "caida_ocupante", df$CLASE)
+unique_accident_class <- unique(accident_class)
+
+# choque
+(two_class <- extract_str(str_vector = unique_accident_class, regex = "[C|c]hoque"))
+df$CLASE <- ifelse(df$CLASE %in% two_class, "choque", df$CLASE)
+
+# Otros
+df$CLASE <- ifelse(df$CLASE == "", "otro", df$CLASE)
+df$CLASE <- tolower(df$CLASE)
+
+
+table(df$CLASE)
+
+## BARRIO
+
 
 ## Saving the files into processed data
+
+
 
 write.csv(train_data, file = "./data/processed/train_data.csv")
 write.csv(test_data, file = "./data/processed/test_data.csv")

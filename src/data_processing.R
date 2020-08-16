670ad7c559e5e6caec167f23f5da880d3a4e923b
santiago <santiago.vasquez@rappi.com>
2020-08-16 11:46:42 -0500

    cleaning data and prepocessed

--- src/data_processing.R
+++ src/data_processing.R
@@ -1,13 +1,7 @@
 # --------------------------- Reading data files -----------------------------
 library(data.table)
+library(dplyr)
 library(stringi)
-
-filePaths <- list.files(file.path("./data/raw"), pattern = 'Acc', full.names = TRUE)
-result <- lapply(filePaths, fread)
-names(result) <- filePaths
-accidentes <- rbindlist(result, use.names = TRUE, idcol = "source")
-accidentes <- accidentes[,source:=NULL]
-
 
 ## All the data files
 raw_files <- list.files('data/raw/')
@@ -24,38 +18,32 @@ data_list <- lapply(data_files, function(x) read.csv(file = x))
 df <- do.call("rbind", data_list)
 
 # --------------------------- data cleansing -----------------------------
-str(df)
 
-## CLASE
-table(df$CLASE)
-unique_accident_class <- unique(accident_class)
+# Drop the columns of the dataframe
+df <- select (df,-c(X,Y,OBJECTID,RADICADO,CBML,MES_NOMBRE,
+                    X_MAGNAMED,Y_MAGNAMED, BARRIO,DIRECCION_ENC))
 
-extract_str <- function(str_vector, regex){
-  idx <- stringi::stri_detect(str = str_vector, regex = regex)
-  return(str_vector[idx])
-}      
-# caida ocupante
-one_class <- extract_str(str_vector = unique_accident_class, regex = "[O|o]cupante$")
-df$CLASE <- ifelse(df$CLASE %in% one_class, "caida_ocupante", df$CLASE)
-unique_accident_class <- unique(accident_class)
+# Lowcase rownames
+df <- mutate_each(df, funs=tolower)
 
-# choque
-(two_class <- extract_str(str_vector = unique_accident_class, regex = "[C|c]hoque"))
-df$CLASE <- ifelse(df$CLASE %in% two_class, "choque", df$CLASE)
+# Change encoding
+df <- df %>% 
+  mutate_if(is.character, function(x){iconv(x, to = "ASCII//TRANSLIT")})
 
-# Otros
-df$CLASE <- ifelse(df$CLASE == "", "otro", df$CLASE)
-df$CLASE <- tolower(df$CLASE)
+# Create column date formart
+df <- df %>% 
+  mutate(FECHA = as.Date.character(FECHA, tryFormats = c("%Y-%m-%d", "%Y/%m/%d")))
 
+# Remove empty rows
+df <- df[df$CLASE!= "", ]
+df <- df[df$DISENO!= "", ]
+df <- df[df$COMUNA!= "", ]
+df <- df[df$COMUNA!= '0', ]
 
-table(df$CLASE)
+# Select data frame training and testing
+train_data <- df[df$PERIODO!= '2018', ]
+test_data <- df[df$PERIODO== '2018', ]
 
-## BARRIO
-
-
-## Saving the files into processed data
-
-
-
+# Save data frame training and testing
 write.csv(train_data, file = "./data/processed/train_data.csv")
 write.csv(test_data, file = "./data/processed/test_data.csv")
